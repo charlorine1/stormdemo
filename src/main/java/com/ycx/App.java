a0d6a99108f0082a63f72b63693e44ad0ff2508a
yang <qq>
2018-11-14 19:11:46 +0800

    master

---
+++ src/main/java/com/ycx/App.java
@@ -0,0 +1,20 @@
+package com.ycx;
+
+import org.junit.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    @Test
+    public void run (){
+        System.out.println("11111111111111");
+    }
+}
